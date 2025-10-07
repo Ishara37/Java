1f3dac041ea0e7231ac1fff1d01b79e5e12055a8
IshuDhakal <isharadhakal15@gmail.com>
2025-10-07 15:42:37 -0500

    Commit Recursion

--- src/Day1/BMICalculator.java
+++ src/Day1/BMICalculator.java
@@ -1,6 +1,7 @@
 package Day1;
 import java.util.Scanner;
 public class BMICalculator {
+
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
         System.out.println("Enter your weight in Kg :");
